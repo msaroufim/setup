db4fbac3d79c2df3755429672f376965abc6c592
Mark Saroufim <marksaroufim@meta.com>
2023-12-16 22:16:23 -0700

    Create m1.sh

---
+++ m1.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# Install ripgrep (rg)
+brew install ripgrep
+# Install fzf (f)
+brew install fzf
+# Install autojump (j)
+brew install autojump
+# Install gh (GitHub CLI)
+brew install gh
+# Install bat (cat)
+brew install bat
+# Install duf (du)
+brew install duf
+# Install exa (ls)
+brew install exa
+# Install neofetch (info)
+brew install neofetch
